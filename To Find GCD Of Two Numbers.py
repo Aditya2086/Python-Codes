d501b4f60be44e779e8f7d70a18cf49c691470ef
Aditya Pratap Singh Chauhan <90275944+Aditya2086@users.noreply.github.com>
2022-07-30 21:06:21 +0530

    Create To Find GCD Of Two Numbers.py

---
+++ To Find GCD Of Two Numbers.py
@@ -0,0 +1,6 @@
+# 1. To write a Python program to find GCD of two numbers.
+
+import math
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("The GCD of the two numbers is",math.gcd(a,b))
